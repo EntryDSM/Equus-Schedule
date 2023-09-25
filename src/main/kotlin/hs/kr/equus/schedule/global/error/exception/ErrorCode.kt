b165a34bb14448b3381ba88be1c22fb8355ef630
seongwon <seongwon1290@dsm.hs.kr>
2023-09-26 08:38:21 +0900

    📑 :: ErrorCode

---
+++ src/main/kotlin/hs/kr/equus/schedule/global/error/exception/ErrorCode.kt
@@ -0,0 +1,13 @@
+package hs.kr.equus.schedule.global.error.exception
+
+enum class ErrorCode(
+    val status: Int,
+    val message: String
+) {
+    // Internal Server Error
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+
+    // UnAuthorization
+    INVALID_TOKEN(401, "Invalid Token"),
+    EXPIRED_TOKEN(401, "Expired Token")
+}
