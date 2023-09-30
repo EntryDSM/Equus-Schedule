927fe697034c78889b09b294a9038fc0e51fed3b
seongwon <seongwon1290@dsm.hs.kr>
2023-09-30 14:18:00 +0900

    📑 :: ConfigurationPropertiesConfig

---
+++ src/main/kotlin/hs/kr/equus/schedule/global/error/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package hs.kr.equus.schedule.global.error.config
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@EnableConfigurationProperties
+@Configuration
+class ConfigurationPropertiesConfig
