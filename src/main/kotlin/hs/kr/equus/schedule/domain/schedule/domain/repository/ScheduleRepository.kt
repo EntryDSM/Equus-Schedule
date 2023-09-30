ad846add96d175a38d7e94c0a1165e9ee7d69842
seongwon <seongwon1290@dsm.hs.kr>
2023-09-30 14:19:53 +0900

    📑 :: ScheduleRepository

---
+++ src/main/kotlin/hs/kr/equus/schedule/domain/schedule/domain/repository/ScheduleRepository.kt
@@ -0,0 +1,12 @@
+package hs.kr.equus.schedule.domain.schedule.domain.repository
+
+import hs.kr.equus.schedule.domain.schedule.domain.Schedule
+import hs.kr.equus.schedule.domain.schedule.domain.types.Type
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface ScheduleRepository : JpaRepository<Schedule, UUID> {
+    fun findAllBy(): List<Schedule>
+
+    fun findByType(type: Type): Schedule?
+}
